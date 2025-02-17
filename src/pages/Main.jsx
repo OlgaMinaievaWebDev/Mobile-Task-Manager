8ec63c2d11170ad04cb6f0cc82e1466686f4e24a
Olga <minaeva9@gmail.com>
2025-02-17 11:28:23 -0500

    buttons for task and board styled

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Main() {
   const navigate = useNavigate();
@@ -18,8 +18,8 @@ function Main() {
   const day = weekday[date.getDay()];
   return (
     <div className="bg-black font-family text-white w-full h-screen">
-      <div className="flex flex-col p-3 gap-3">
-        <h1 className="text-6xl text-blue ">
+      <div className="flex flex-col p-4 gap-3">
+        <h1 className="text-6xl text-blue mb-3 ">
           Good {hours < 12 ? "Morning" : "Evening"}
         </h1>
         <div className="flex justify-between">
@@ -33,11 +33,20 @@ function Main() {
           </div>
         </div>
         <div className="flex justify-between">
-          <button className="text-black bg-white hover:bg-blue-800 focus:ring-3 focus:outline-none focus:ring-blue-300 rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            12
-          </button>
-          <button onClick={() => navigate("/task/1")}>Task</button>
-          <button onClick={() => navigate("/boards")}>Boards</button>
+          <div className="flex content-center">
+            <button className="text-black bg-white focus:ring-2 focus:outline-none focus:ring-blue rounded-full text-sm p-2 text-center mr-2 ">
+              12
+            </button>
+            <button onClick={() => navigate("/task/1")} className="text-2xl">
+              Task
+            </button>
+          </div>
+          <div className="flex content-center">
+            <button className="text-white border-white border-1 bg-black focus:ring-2 focus:outline-none focus:ring-blue rounded-full text-sm p-2 text-center mr-2 ">
+              3
+            </button>
+            <button onClick={() => navigate("/boards")}>Boards</button>
+          </div>
         </div>
         <div className="h-1 bg-white w-full"></div>
         <div className="flex justify-between">
