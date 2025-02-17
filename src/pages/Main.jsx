d8251ed203f2caf27f3879607a1f5fa43c1f2b86
Olga <minaeva9@gmail.com>
2025-02-17 11:52:19 -0500

    add logic for greeting, display days of week

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -16,11 +16,21 @@ function Main() {
     "Saturday",
   ];
   const day = weekday[date.getDay()];
+  const todayDay = new Date().toLocaleString("en-US", { weekday: "long" });
   return (
     <div className="bg-black font-family text-white w-full h-screen">
       <div className="flex flex-col p-4 gap-3">
         <h1 className="text-6xl text-blue mb-3 ">
-          Good {hours < 12 ? "Morning" : "Evening"}
+          Good{" "}
+          {hours < 5
+            ? "Night"
+            : hours < 12
+            ? "Morning"
+            : hours < 18
+            ? "Afternoon"
+            : hours < 22
+            ? "Evening"
+            : "Night"}
         </h1>
         <div className="flex justify-between">
           <div>
@@ -32,29 +42,51 @@ function Main() {
             <p className="text-sm opacity-50">Completed Tasks</p>
           </div>
         </div>
-        <div className="flex justify-between">
-          <div className="flex content-center">
-            <button className="text-black bg-white focus:ring-2 focus:outline-none focus:ring-blue rounded-full text-sm p-2 text-center mr-2 ">
-              12
-            </button>
-            <button onClick={() => navigate("/task/1")} className="text-2xl">
+        <div className="flex justify-between mt-5">
+          <div>
+            <button
+              onClick={() => navigate("/task/1")}
+              className="flex items-center text-white text-2xl"
+            >
+              <span className="border border-white rounded-2xl px-2 py-1 mr-2 text-sm">
+                12
+              </span>
               Task
             </button>
           </div>
-          <div className="flex content-center">
-            <button className="text-white border-white border-1 bg-black focus:ring-2 focus:outline-none focus:ring-blue rounded-full text-sm p-2 text-center mr-2 ">
-              3
+          <div>
+            <button
+              onClick={() => navigate("/boards")}
+              className="flex items-center text-white text-2xl"
+            >
+              <span className="border border-white rounded-2xl px-2 py-1 mr-2 text-sm">
+                3
+              </span>
+              Boards
             </button>
-            <button onClick={() => navigate("/boards")}>Boards</button>
           </div>
         </div>
-        <div className="h-1 bg-white w-full"></div>
+        <div>
+          <progress className="h-1 bg-white w-full" />
+        </div>
         <div className="flex justify-between">
           <button>Boards</button>
           <div>
             <button>Active</button>
             <button>Done</button>
           </div>
+        </div>
+        <div className="flex justify-evenly gap-3">
+          {weekday.map((day) => (
+            <p
+              key={day}
+              className={
+                day === todayDay ? "text-blue font-bold" : "text-gray-400"
+              }
+            >
+              {day.slice(0, 3)}
+            </p>
+          ))}
         </div>
       </div>
     </div>
